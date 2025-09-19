457d81545a6f68f435d665e027bfc364195f40b5
Haseeb Ahmed <malikhaseeb0912736@gmail.com.com>
2025-09-19 11:07:01 +0500

    megablog

--- 12MegaBlog/src/appwrite/auth.js
+++ 12MegaBlog/src/appwrite/auth.js
@@ -1,9 +1,11 @@
+import { useState } from 'react';
 import conf from '../conf/conf.js';
 import { Client, Account, ID } from "appwrite";
 
 
 
 export class AuthService {
+    
     client = new Client();
     account;
 
--- 12MegaBlog/src/component/Login.jsx
+++ 12MegaBlog/src/component/Login.jsx
@@ -7,6 +7,7 @@ import authService from "../appwrite/auth"
 import {useForm} from "react-hook-form"
 
 function Login() {
+
     const navigate = useNavigate()
     const dispatch = useDispatch()
     const {register, handleSubmit} = useForm()
